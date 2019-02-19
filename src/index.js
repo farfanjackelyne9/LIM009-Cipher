336d482c1e62409b4c77a3609364c20982f0f686
farfanjackelyne9 <farfanjackelyne9@gmail.com>
2019-02-19 11:14:25 -0500

    estructura html del cifrado cesar

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 /* Acá va tu código */
+const btningresar=document.getElementById('btningresar');
+btningresar.addEventListener("click",()=>{
+  pagina1.style.display="none";
+  pagina2.style.display="block";
+})
+const number=document.getElementById('number').value;
