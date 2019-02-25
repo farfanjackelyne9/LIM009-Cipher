19a3c0b525e12825902729dc44ff19713c83bba6
farfanjackelyne9 <farfanjackelyne9@gmail.com>
2019-02-25 09:50:56 -0500

    implementacion de cifrado

--- src/cipher.js
+++ src/cipher.js
@@ -1,7 +1,17 @@
-window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
-    
+window.cipher = {                                   // exportar del elemento window
+  encode: (textoDescifrado,desplazamiento) => {
+   /* Acá va tu código */
+  let ascii;
+  let resultado=" ";
+
+   for(let i = 0 ; i < textoDescifrado.length ; i++){
+     console.log(ascii =textoDescifrado[i].charCodeAt());
+    if( 65 <= ascii <= 90 ){
+     nuevoResultado = String.fromCharCode((ascii - 65 + desplazamiento) % 26 + 65);
+    console.log(resultado += nuevoResultado);
+  }
+  }
+   return resultado;
   },
   decode: () => {
     /* Acá va tu código */
--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 /* Acá va tu código */
 const btningresar=document.getElementById('btningresar');
 btningresar.addEventListener("click",()=>{
-  pagina1.style.display="none";
-  pagina2.style.display="block";
+pagina1.style.display="none";
+pagina2.style.display="block";
 })
-const number=document.getElementById('number').value;
+let textoDescifrado;
+let desplazamiento;
+let nuevoResultado;
+
+document.getElementById('btncifrar').addEventListener("click",()=>{
+textoDescifrado=document.getElementById('descifrado').value;
+desplazamiento=parseInt(document.getElementById('number').value);
+resultado=cipher.encode(textoDescifrado,desplazamiento);
+document.getElementById('cifrado').value= resultado;
+})
