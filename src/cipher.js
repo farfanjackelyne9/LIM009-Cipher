0d55f164db4db6053776759581c0b74c2834dfe1
farfanjackelyne9 <farfanjackelyne9@gmail.com>
2019-02-26 12:08:38 -0500

    implementacion de descifrado

--- src/cipher.js
+++ src/cipher.js
@@ -1,19 +1,39 @@
-window.cipher = {                                   // exportar del elemento window
+window.cipher = {
   encode: (textoDescifrado,desplazamiento) => {
-   /* Acá va tu código */
-  let ascii;
-  let resultado=" ";
-
-   for(let i = 0 ; i < textoDescifrado.length ; i++){
-     console.log(ascii =textoDescifrado[i].charCodeAt());
-    if( 65 <= ascii <= 90 ){
-     nuevoResultado = String.fromCharCode((ascii - 65 + desplazamiento) % 26 + 65);
-    console.log(resultado += nuevoResultado);
-  }
-  }
-   return resultado;
-  },
-  decode: () => {
     /* Acá va tu código */
+   let asciiDescifrado;
+   let nuevoResultadoCifrado;
+   let resultadoCifrado=" ";
+
+    for(let i = 0 ; i < textoDescifrado.length ; i++){
+      asciiDescifrado =textoDescifrado[i].charCodeAt();
+     if( 65 <= asciiDescifrado <= 90 ){
+      nuevoResultadoCifrado = String.fromCharCode((asciiDescifrado - 65 + desplazamiento) % 26 + 65);
+     console.log(resultadoCifrado += nuevoResultadoCifrado);
+      }
+    }
+    return resultadoCifrado;
+  },
+
+  decode: (textoCifrado,desplazamiento) => {
+    /* Acá va tu código */
+    let nuevoNumero;
+    let nuevoResultadoDescifrado;
+    let nuevoNumeroCifrado;
+    let asciiCifrado;
+    let resultadoDescifrado=" ";
+
+   for(let i = 0 ; i < textoCifrado.length ; i++){
+     asciiCifrado=textoCifrado[i].charCodeAt();
+       if( 65 <= asciiCifrado<=90){
+         nuevoNumero=(asciiCifrado-65-desplazamiento);
+        }  while (nuevoNumero < 0){
+          nuevoNumero=nuevoNumero + 26;
+          }
+          nuevoNumeroCifrado=nuevoNumero%26+65
+          console.log(nuevoResultadoDescifrado=String.fromCharCode(nuevoNumeroCifrado))
+          resultadoDescifrado += nuevoResultadoDescifrado;
+     }
+    return resultadoDescifrado;
   }
-};
+}
--- src/index.js
+++ src/index.js
@@ -4,13 +4,22 @@ btningresar.addEventListener("click",()=>{
 pagina1.style.display="none";
 pagina2.style.display="block";
 })
+
 let textoDescifrado;
 let desplazamiento;
-let nuevoResultado;
+let textoCifrado;
 
 document.getElementById('btncifrar').addEventListener("click",()=>{
 textoDescifrado=document.getElementById('descifrado').value;
 desplazamiento=parseInt(document.getElementById('number').value);
-resultado=cipher.encode(textoDescifrado,desplazamiento);
-document.getElementById('cifrado').value= resultado;
+resultadoCifrado=cipher.encode(textoDescifrado,desplazamiento);
+document.getElementById('cifrado').value= resultadoCifrado;
+})
+
+document.getElementById('btndescifrar').addEventListener("click",()=>{
+textoCifrado=document.getElementById('cifrado').value;
+desplazamiento=parseInt(document.getElementById('number').value);
+resultadoDescifrado=cipher.decode(textoCifrado,desplazamiento);
+document.getElementById('descifrado').value=resultadoDescifrado;
+
 })
